f38dc98bed0841b488b435ce5542f2fd747f5d94
kart-c <kartikeyachoudhary11@gmail.com>
2023-07-08 14:26:01 +0530

    feat: reroute to login or go to tables if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { useAuth } from '@hooks/useAuth';
+import { ROUTES } from '@lib/routes';
+
 export default function Home() {
+  const router = useRouter();
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace(ROUTES.CHECKWEIGHER.STANDARD);
+    } else {
+      router.replace(ROUTES.LOGIN);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoggedIn]);
+
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      hello world
+    <main className="flex items-center justify-center main-height">
+      <div className="w-12 h-12 border-r-2 rounded-full border-r-gray-700 animate-spin" />
     </main>
   );
 }
